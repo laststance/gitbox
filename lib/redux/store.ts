7d8713232be00791efca3f8bd696cfcb38c75d85
Ryota Murakami <dojce1048@gmail.com>
2025-12-23 11:14:43 +0900

    fix(lint): disable import/order for entire store.ts due to CI/local path resolution inconsistency

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -11,12 +11,12 @@
  * RTK Query for security reasons (HTTP-only cookie token access).
  */
 
+/* eslint-disable import/order -- Workspace package @gitbox/* resolves differently in CI vs local environments */
 import { configureStore } from '@reduxjs/toolkit'
 import type { TypedUseSelectorHook } from 'react-redux'
 import { useDispatch, useSelector } from 'react-redux'
-// eslint-disable-next-line import/order -- Workspace package resolution differs between local and CI environments
-import { createStorageMiddleware } from '@gitbox/redux-storage-middleware'
 
+import { createStorageMiddleware } from '@gitbox/redux-storage-middleware'
 import authReducer from './slices/authSlice'
 import boardReducer from './slices/boardSlice'
 import settingsReducer from './slices/settingsSlice'
